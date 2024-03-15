ebb9bec0385944370a19ec6bbe33a65e4dd91556
dmytrovb02 <d.vyshnivskyi@gmail.com>
2024-03-15 07:40:14 +0200

    Initial commit

---
+++ src/main/java/com/utilitytracker/UtilityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.utilitytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utilitytracker/UtilityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilitytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
